04e549db7dbe8bcc5b431ff24a4b97c11683fc5e
Steve Kite <steve@actor.im>
2016-02-25 21:06:08 +0300

    fix(desktop): Fixing desktop icon

--- app-desktop/build.sh
+++ app-desktop/build.sh
@@ -1,26 +1,19 @@
-if ! npm list -g --depth=0 electron-packager &> /dev/null ; then 
-	echo "installing electron-packager"
-	npm i electron-packager -g
-fi 
-
 mkdir build
 mkdir build/out
 
-echo "Building OSX"
+echo "Building All Platforms"
 
-electron-packager src Actor --platform=darwin --arch=x64 --app-bundle-id=im.actor.app --version=0.36.0 --out=build --icon=icon/osx.icns
+electron-packager src Actor --platform=all --arch=all --app-bundle-id=im.actor.app --version=0.36.8 --out=build --icon=assets/app_icon
 
 cd build/Actor-darwin-x64/
-
-
 
 zip --symlinks -r Actor-Mac.zip Actor.app/
 mv Actor-Mac.zip ../out/
 cd ../../
 
-echo "Building Windows"
+# echo "Building Windows"
 
-electron-packager src Actor --platform=win32 --arch=all --app-bundle-id=im.actor.app --version=0.36.0 --out=build --icon=icon/windows.icns
+# electron-packager src Actor --platform=win32 --arch=all --app-bundle-id=im.actor.app --version=0.36.8 --out=build --icon=icon/windows.icns
 
 cd build
 zip --symlinks -r Actor-Win-x86.zip Actor-win32-ia32/
@@ -30,8 +23,8 @@ zip --symlinks -r Actor-Win-x64.zip Actor-win32-x64/
 mv Actor-Win-x64.zip out/
 cd ../
 
-echo "Building Linux"
-electron-packager src Actor --platform=linux --arch=all --app-bundle-id=im.actor.app --version=0.36.0 --out=build
+# echo "Building Linux"
+# electron-packager src Actor --platform=linux --arch=all --app-bundle-id=im.actor.app --version=0.36.8 --out=build
 
 cd build
 zip --symlinks -r Actor-Linux-x86.zip Actor-linux-ia32/
