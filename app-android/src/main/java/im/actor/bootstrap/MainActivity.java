7f1d0527d4e4eda09a16489de5e6c404f8047381
kor_ka <badgrib@gmmail.com>
2016-08-04 20:45:01 +0300

    chore(android): bump sdk

--- app-android/src/main/java/im/actor/bootstrap/MainActivity.java
+++ app-android/src/main/java/im/actor/bootstrap/MainActivity.java
@@ -1,17 +1,17 @@
 package im.actor.bootstrap;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
 
 import im.actor.sdk.ActorSDK;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 	
-	ActorSDK.sharedActor().waitForReady();
+	    ActorSDK.sharedActor().waitForReady();
         
         ActorSDK.sharedActor().startMessagingApp(this);
         finish();
